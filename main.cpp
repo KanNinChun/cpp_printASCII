a34158406da8b612c3a6fc76d4a2faba009460d4
KanNinChun <210226456@stu.vtc.edu.hk>
2022-05-29 17:31:41 +0800

    add welcome function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
 
+using namespace std;
+
 void Gen()
 {
     // should start from 33 aned end at 126 , because 32 is a space bar and 127 mean Delete
@@ -10,10 +12,23 @@ void Gen()
     }
 }
 
+void Welcome()
+{
+    system("cls"); // clean everything from console
+    std::string username; // set up a new var
+
+    std::cout<<"Enter Your Name: " <<endl;
+    std::cin >> username; // input
+    Sleep(600); 
+    system("cls"); // clean everything from console
+    std::cout<<"Welcome "<< "To the Code House, " << username << "\n" <<std::endl;
+    std::cout<<"Enjoy your coding journey~ Have Fun with it." <<std::endl;
+}
 
 int main()
 {
     Gen(); //calling function
-    std::cin.get(); // waitting for inut
+    Welcome(); //calling function
+    std::cin.get(); // waitting for input(pasue console for us to see)
     return 0; 
 }
